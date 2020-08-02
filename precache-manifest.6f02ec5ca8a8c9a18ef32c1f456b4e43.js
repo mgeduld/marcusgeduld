152c448dd6c435190eb6139f2a9ea817e7ca67e5
Marcus Geduld <mgeduld@gmail.com>
2020-08-02 14:22:14 -0400

    Updates

---
+++ precache-manifest.6f02ec5ca8a8c9a18ef32c1f456b4e43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4448df8c77d22d3ecc92777193e7429c",
+    "url": "/marcusgeduld/index.html"
+  },
+  {
+    "revision": "3bf85012a7f761b23b7f",
+    "url": "/marcusgeduld/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9df97e4beb79faddb9f4",
+    "url": "/marcusgeduld/static/js/2.846e0fef.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/marcusgeduld/static/js/2.846e0fef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bf85012a7f761b23b7f",
+    "url": "/marcusgeduld/static/js/main.dc8e1e93.chunk.js"
+  },
+  {
+    "revision": "9551f98f6b22ad1a7fc5",
+    "url": "/marcusgeduld/static/js/runtime-main.ababbe59.js"
+  }
+]);
